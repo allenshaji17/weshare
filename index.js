c83860977c6332fdc0b4e600d3029daa863aec7e
allenshaji17 <52339640+allenshaji17@users.noreply.github.com>
2021-07-15 21:16:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const emailForm = document.querySelector("#emailForm");
 
 const toast = document.querySelector(".toast");
 
-const baseURL = "https://we-share-file-transfer.herokuapp.com/";
+const baseURL = "https://we-share-file-transfer.herokuapp.com";
 const uploadURL = `${baseURL}/files/api`;
 const emailURL = `${baseURL}/api/files/send`;
 
